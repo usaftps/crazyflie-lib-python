6b63e1b2001caeaa02c07614fc96230840a73ab4
slab14 <mmccorm1@andrew.cmu.edu>
2022-01-15 20:43:58 -0800

    reworking to use pynput, no need for increased privs

--- examples/slabs_world/keyboard_commands.py
+++ examples/slabs_world/keyboard_commands.py
@@ -1,4 +1,3 @@
-import keyboard
 import logging
 import time
 import cflib.crtp
@@ -8,6 +7,7 @@ from cflib.crazyflie.syncCrazyflie import SyncCrazyflie
 from cflib.positioning.motion_commander import MotionCommander
 from cflib.positioning.position_hl_commander import PositionHlCommander
 from cflib.utils import uri_helper
+from pynput import keyboard
 
 URI = uri_helper.uri_from_env(default='radio://0/80/2M/E7E7E7E7E7')
 DEFAULT_HEIGHT = 0.05
@@ -17,6 +17,37 @@ position_estimate = [0, 0]
 
 logging.basicConfig(level=logging.ERROR)
 
+
+def on_release(key, scf):
+    '''
+    print('{0} released'.format(key))
+    '''
+    with MotionCommander(scf, default_height=0.5) as mc:
+        if key == keyboard.Key.esc:
+            # Stop listener
+            return False
+        if key == keyboard.Key.up:
+            print('up arrow, go up')
+            mc.up(DEFAULT_HEIGHT)
+        elif key == keyboard.Key.down:
+            print('down arrow, go down')
+            mc.down(DEFAULT_HEIGHT)            
+        elif key == keyboard.KeyCode.from_char('w'):
+            print('w, go forwards')
+            mc.forward(DEFAULT_TRANSLATE)            
+        elif key == keyboard.KeyCode.from_char('s'):
+            print('s, go backward')
+            mc.back(DEFAULT_TRANSLATE)
+        elif key == keyboard.KeyCode.from_char('a'):
+            print('a, go left')
+            mc.left(DEFAULT_TRANSLATE)
+        elif key ==keyboard.KeyCode.from_char('d'):
+            print('d, go right')
+            mc.right(DEFAULT_TRANSLATE)
+
+
+def keyboard_test():
+    a=1
 
 def keyboard_inputs(scf):
     print("get keyboard inputs")
@@ -66,8 +97,8 @@ if __name__ == '__main__':
         scf.cf.param.add_update_callback(group='deck', name='bcFlow2',
                                          cb=param_deck_flow)
     time.sleep(2)
-    if is_deck_attached:
-        print("do I get here?")
-        keyboard_inputs(scf)
 
-    print('failing here')
+    if is_deck_attached:
+        with keyboard.Listener(on_release=lambda event:on_release(event, scf)) as listener:
+            listener.join()
+        print('failing here')
