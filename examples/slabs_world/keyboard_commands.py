5a5a4b42cfcecfb9c6f26b037874a21db6a38759
slab14 <mmccorm1@andrew.cmu.edu>
2022-01-16 19:55:40 -0800

    min working example

--- examples/slabs_world/keyboard_commands.py
+++ examples/slabs_world/keyboard_commands.py
@@ -18,33 +18,23 @@ position_estimate = [0, 0]
 logging.basicConfig(level=logging.ERROR)
 
 
-def on_release(key, scf):
-    print('got a key press')
-    '''
-    print('{0} released'.format(key))
-    '''
-    with MotionCommander(scf, default_height=0.5) as mc:
-        if key == keyboard.Key.esc:
-            # Stop listener
-            return False
-        if key == keyboard.Key.up:
-            print('up arrow, go up')
-            mc.up(DEFAULT_HEIGHT)
-        elif key == keyboard.Key.down:
-            print('down arrow, go down')
-            mc.down(DEFAULT_HEIGHT)            
-        elif key == keyboard.KeyCode.from_char('w'):
-            print('w, go forwards')
-            mc.forward(DEFAULT_TRANSLATE)            
-        elif key == keyboard.KeyCode.from_char('s'):
-            print('s, go backward')
-            mc.back(DEFAULT_TRANSLATE)
-        elif key == keyboard.KeyCode.from_char('a'):
-            print('a, go left')
-            mc.left(DEFAULT_TRANSLATE)
-        elif key ==keyboard.KeyCode.from_char('d'):
-            print('d, go right')
-            mc.right(DEFAULT_TRANSLATE)
+def on_release(key, mc):
+    if key == keyboard.Key.esc:
+        # Stop listener
+        print('all done')
+        return False
+    if key == keyboard.Key.up:
+        mc.up(DEFAULT_HEIGHT)
+    elif key == keyboard.Key.down:
+        mc.down(DEFAULT_HEIGHT)            
+    elif key == keyboard.KeyCode.from_char('w'):
+        mc.forward(DEFAULT_TRANSLATE)            
+    elif key == keyboard.KeyCode.from_char('s'):
+        mc.back(DEFAULT_TRANSLATE)
+    elif key == keyboard.KeyCode.from_char('a'):
+        mc.left(DEFAULT_TRANSLATE)
+    elif key ==keyboard.KeyCode.from_char('d'):
+        mc.right(DEFAULT_TRANSLATE)
 
 
 def param_deck_flow(name, value_str):
@@ -61,10 +51,11 @@ if __name__ == '__main__':
     with SyncCrazyflie(URI, cf=Crazyflie(rw_cache='./cache')) as scf:
         scf.cf.param.add_update_callback(group='deck', name='bcFlow2',
                                          cb=param_deck_flow)
-        time.sleep(2)
+        time.sleep(1)
         if is_deck_attached:
             print('let\'s fly')
-            with keyboard.Listener(on_release=lambda event:on_release(event, scf)) as listener:
-                listener.join()
-                print('waiting...')
+            with MotionCommander(scf, default_height=0.5) as mc:
+                with keyboard.Listener(on_release=lambda event:on_release(event, mc)) as listener:
+                    listener.join()
+                    print('waiting...')
             print('failing here')
